9a09eaf4dd7da13b0f82d81e8926e875776e8e4d
bogdandeac <bogdan.deac@toptal.com>
2025-03-15 23:31:15 +0200

    Cleanup trie implementation

--- common/trie/trie.go
+++ common/trie/trie.go
@@ -8,23 +8,21 @@ type Trie[T comparable, V any] struct {
 }
 
 func NewTrie[T comparable, V any]() *Trie[T, V] {
-	return &Trie[T, V]{}
+	return &Trie[T, V]{
+		Children: make(map[T]*Trie[T, V]),
+	}
 }
 
 func (t *Trie[T, V]) Insert(elems []T, value V) error {
 	crt := t
-	elIndex := 0
-	for elIndex < len(elems) {
-		if len(crt.Children) == 0 {
-			crt.Children = make(map[T]*Trie[T, V])
-		}
-		child, ok := crt.Children[elems[elIndex]]
+
+	for _, elem := range elems {
+		child, ok := crt.Children[elem]
 		if !ok {
 			child = NewTrie[T, V]()
-			crt.Children[elems[elIndex]] = child
+			crt.Children[elem] = child
 		}
 		crt = child
-		elIndex++
 	}
 	if crt.Value != nil {
 		return errors.New("Value already exists")
